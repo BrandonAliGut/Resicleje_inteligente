2e84ed304dd07f3979c7712e47abcaee06814744
Brandon <90984100+BrandonAliGut@users.noreply.github.com>
2023-09-19 12:46:25 -0600

    Update views.py

--- Frontend_resiclaje/views.py
+++ Frontend_resiclaje/views.py
@@ -5,7 +5,7 @@ from Api_Reciclaje_I.models import Category
 from django.utils import timezone
 
 def categoryView(request):
-    return render(request, 'Home/Index_data.html')
+    return render(request, 'Home/Ihtmviwbase.html')
 """   
 class CategoryView(ListView):
     model = Category
