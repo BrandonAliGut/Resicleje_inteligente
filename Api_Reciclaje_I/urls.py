84482582b68b28219b3e47fe9b97191c7dd917c3
Brandon <bg4710699@gmail.com>
2023-09-24 12:57:50 -0600

    Cors

--- Api_Reciclaje_I/urls.py
+++ Api_Reciclaje_I/urls.py
@@ -6,8 +6,8 @@ from rest_framework.routers import DefaultRouter
 router.register('api/Category', Category_Api, 'Category')"""
 
 urlpatterns = [
-    path('api/Category', Category_Api.as_view()),
-    path('api/Category/<int:pk>/', categoria_detail, name="update_del_category"), 
+    path('api/categories', Category_Api.as_view()),
+    path('api/categories/<int:pk>/', categoria_detail, name="update_del_category"), 
     
     
 ] 
