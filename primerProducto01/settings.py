f7dd218155d7d704c2de8428548c4864910d0424
Brandon <bg4710699@gmail.com>
2023-09-24 13:21:56 -0600

    Cors

--- primerProducto01/settings.py
+++ primerProducto01/settings.py
@@ -74,9 +74,10 @@ CORS_ORIGIN_ALLOW_ALL"""
 
 CORS_ALLOW_ALL_ORIGINS = True
 ORS_ALLOW_ALL_ORIGINS = True
-'''CORS_ALLOWED_ORIGINS = [
+
+CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',
-]'''
+]
 
 
 
