14bc6f11905f4af181ca7e57fa3c77b9a142d029
Brandon <bg4710699@gmail.com>
2023-10-01 11:34:36 -0600

    update_category

--- Api_Reciclaje_I/models.py
+++ Api_Reciclaje_I/models.py
@@ -7,7 +7,7 @@ import datetime
 
 class Category(models.Model):
     name= models.CharField(max_length=50, blank=False)
-    img= CloudinaryField("Img_reciclaje_app")
+    img= CloudinaryField("Img_reciclaje_app", blank=True)
     information  = models.TextField()
     created_at = models.DateField( auto_now_add=True, editable=False)
     update_at = models.DateField(default=datetime.datetime.now())
--- Api_Reciclaje_I/serializers.py
+++ Api_Reciclaje_I/serializers.py
@@ -7,3 +7,17 @@ class SerializerCategoria(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ['id','name','img', 'information','update_at','created_at']
+        
+    def update(self, instance, validated_data):
+        img = validated_data.get('img')
+        if img and img != "":
+            instance.img = validated_data.get('img', instance.img)
+        
+        instance.name = validated_data.get('name', instance.name)
+        instance.information = validated_data.get('information', instance.information)
+        instance.update_at = validated_data.get('update_at', instance.update_at)
+        instance.created_at = validated_data.get('created_at', instance.created_at)
+
+        instance.save()
+        
+        return instance
